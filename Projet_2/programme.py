ddaa274276f14ccf1fb6a0fdd5a13f1cb81ccd08
agross <alexia.gross@insa-rouen.fr>
2021-04-07 16:57:41 +0200

    Ajout de labels traités en fct du XSD du projet 1. Ajout d'une autre sheet 'RC0015' pour tester et ça récupère bien toutes les données

--- Projet_2/programme.py
+++ Projet_2/programme.py
@@ -2,29 +2,64 @@
 from pandas_ods_reader import read_ods
 from lxml import etree
 import unidecode
+import struct
 
 
-def creationRacine(df):
+def creationRacine(path):
+    sheet_name = "Classes"
+   
     root = etree.Element("root")
-    doc = etree.SubElement(root, "EntiteDuMondeDeLaFormation")
+    racine = etree.SubElement(root, "EntiteDuMondeDeLaFormation")
 
-    for j in range(len(df)):
-        
-        doc = etree.SubElement(root, "EntiteDuMondeDeLaFormation")
+    obtenirValeurCellule(path, sheet_name, racine, 1)
+    creationFils(path, racine)
+    contenuSheetSecondaire(path, root)
 
-        for i in range(len(df.columns)): 
-            nom=df.columns[i].replace(" ", "")
-            etree.SubElement(doc, unidecode.unidecode(nom)).text = df.iloc[j,i]
 
     tree = etree.ElementTree(root)
-    tree.write("filename.xml")
+    return(tree)
+
+
+def creationFils(path, branche):
+    nomFils=['CertificatBadgeDiplome', 'Certification', 'NiveauDeCertification', 'ConditionDAcces']
+    sheet_name = "Classes"
+    df = read_ods(path, sheet_name)
+
+    for j in range(len(nomFils)):
+        fils = etree.SubElement(branche, nomFils[j])
+        obtenirValeurCellule(path, "Classes", fils, j+2)
+
+
+def contenuSheetSecondaire(path, branche):
+    detailfils = etree.SubElement(branche, "DetailsDesIdentifiants")
+    sheet_name = "RC0015"
+    descriptifParId= etree.SubElement(detailfils, "Feuille")
+
+    df = read_ods(path, sheet_name)
+
+    for j in range(len(df)):
+        obtenirValeurCellule(path, sheet_name, descriptifParId, j)
+
+
+
+def obtenirValeurCellule(path, sheet_name, branche, nb):
+    df = read_ods(path, sheet_name)
+    
+    for i in range(len(df.columns)): 
+        nom=df.columns[i].replace(" ", "") #Permet d'enlever les espaces dans le nom des colonnes
+        if isinstance(df.iloc[nb,i], float):
+            etree.SubElement(branche, unidecode.unidecode(nom)).text = str(df.iloc[nb,i]).encode('utf-8','ignore')
+        else :
+            etree.SubElement(branche, unidecode.unidecode(nom)).text = df.iloc[nb,i]
+
 
 def main():
     path = "NoDEfr-2.ods"
-    sheet_name = "Classes"
-    df = read_ods(path, sheet_name)
-    creationRacine(df)
-    
+    tree = creationRacine(path)
+
+    tree.write("filename.xml")
+
+
 
 if __name__ == "__main__":
     main()
